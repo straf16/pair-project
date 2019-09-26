679a574deb4e8afbf4e6c937a34dd2de4e6b47d3
straf16 <depoksugutamu621@gmail.com>
2019-09-27 02:39:35 +0700

    deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const nodemailer = require('nodemailer')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 4000
 const countSeats = require('./helpers/countSeats')
 const indexRoutes = require('./routes')
 app.use( express.static("public"));
